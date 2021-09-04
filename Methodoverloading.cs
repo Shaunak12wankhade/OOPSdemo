b3ae33497a9351b3f9d7a204e85b0bace74b5e30
unknown <shaunak12wankhade@ggmail.com>
2021-09-04 13:27:18 +0530

    [Shaunak] Adding OOPS Project

---
+++ Methodoverloading.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsdemo
+{
+    class Methodoverloading_
+    {
+        static int PlusMethodInt(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethodDouble(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            int myNum1 = PlusMethodInt(6, 10);
+            double myNum2 = PlusMethodDouble(5.5, 5.8);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+        }
+    }
+}
+
+
